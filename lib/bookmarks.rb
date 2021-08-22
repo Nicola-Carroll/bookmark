b137088fd22187634c75441a32b4025a00d15b35
Nicola Carroll <nicolaccarroll@gmail.com>
2021-08-22 18:34:20 +0100

    orm

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -1,4 +1,4 @@
-require 'pg'
+require_relative 'database_connection'
 
 class Bookmarks
 
@@ -11,31 +11,12 @@ class Bookmarks
   end
 
   def self.add(url:, title:)
-    if ENV['RACK_ENV'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      connection = PG.connect(dbname: 'bookmark_manager')
-    end
-    connection.exec("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}' );")
-  end
-
-  def initialize
-    @bookmarks_db_connection = connect_to_db
+    DatabaseConnection.query("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}');")
   end
 
   def all
-    data = @bookmarks_db_connection.exec('SELECT * FROM bookmarks')
+    data = DatabaseConnection.query('SELECT * FROM bookmarks')
     data.map { |bookmark| {url: bookmark["url"], title: bookmark["title"] } }
   end
 
-
-  private
-
-  def connect_to_db
-    if ENV['RACK_ENV'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      connection = PG.connect(dbname: 'bookmark_manager')
-    end
-  end
 end
